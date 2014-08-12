efc2f9aeccee93b784bd11071e800ba6a9fad3e6
wangfrombupt <wyl@bupt.edu.cn>
2014-08-12 11:56:18 +0800

    This is the code
    Code includes FastMatrixDisguising in the paper "An Efficient And
    Tunable Matrix Disguising Method Toward Privacy Preserving Computation"
    and four existing disguising algorithms for comparing.

--- Matrix Disguising Workstation/Two local algorithms for comparing/Atallah96/Atallah96.m
+++ Matrix Disguising Workstation/Two local algorithms for comparing/Atallah96/Atallah96.m
@@ -1,10 +1,10 @@
 function [ res,n,D ] = Atallah96( A )
 
-% 'Atallah96' is a local matrix disguising algorithm. The detail infomation of
-% 'Atallah96' is in the paper:
+% 'Atallah96' is a local matrix disguising algorithm. 
+% The detail infomation of 'Atallah96' is in the paper:
 
 % "Atallah, M. J.; Pantazopoulos, K. N. & Spafford, E. H. Secure
-% outsourcing of some computations, Computer Science Technical Reports, 1996 "
+% outsourcing of some computations, Computer Science Technical Reports, 1996"
 
 % 'A' is the matrix to be disguised, 'res' is the result of the disguising, and
 % matrix 'D' is the key and it is reversible.
